e4b2de79f146017983ab9634ac883866343ca031
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-07-11 16:48:48 -0300

    req 1

--- inventory_report/inventory/product.py
+++ inventory_report/inventory/product.py
@@ -25,3 +25,4 @@ class Product:
             f" até {self.data_de_validade}"
             f" precisa ser armazenado {self.instrucoes_de_armazenamento}."
         )
+
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,14 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(1, 'mouse', 'Logitech', '10/01/2020', '10/01/2030','123456', 'Armazenar em ambiente arejado sem contato direto com calor')
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'mouse'
+    assert product.nome_da_empresa == 'Logitech'
+    assert product.data_de_fabricacao == '10/01/2020'
+    assert product.data_de_validade == '10/01/2030'
+    assert product.numero_de_serie == '123456'
+    assert product.instrucoes_de_armazenamento == 'Armazenar em ambiente arejado sem contato direto com calor'
+
