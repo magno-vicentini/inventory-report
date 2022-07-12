5baad816e1dbe7bd434cdcfb27f0dea8c3f47900
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-07-12 14:51:39 -0300

    fix: simple report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,39 +1,35 @@
-from datetime import datetime
+from datetime import date
+from collections import Counter
 
 
-class SimpleReport:
-    @staticmethod
-    def search_by_oldest_fabrication(list_products):
-        oldest_date = datetime.now().strftime('%Y-%m-%d')
-        for product in list_products:
-            if product['data_de_fabricacao'] < oldest_date:
-                oldest_date = product['data_de_fabricacao']
-        return oldest_date
-
-    @staticmethod
-    def search_by_nearest_fabrication(list_products):
-        nearest_date = datetime.now().strftime('%Y-%m-%d')
-        date_list = []
-        for product in list_products:
-            if product['data_de_validade'] > nearest_date:
-                date_list.append(product['data_de_validade'])
-        date_list.sort()
-        return date_list
-
-    @staticmethod
-    def search_by_stock_quantity(list_products):
-        list_companies = [product_element['nome_da_empresa']
-                          for product_element in list_products]
-        return max(list_companies)
-
+class SimpleReport():
     @classmethod
     def generate(cls, all_products):
-        old_fabrication = cls.search_by_oldest_fabrication(all_products)
-        near_fabrication = cls.search_by_nearest_fabrication(all_products)[0]
-        stock_company = cls.search_by_stock_quantity(all_products)
-        generate_string = (
-            f"Data de fabricação mais antiga: {old_fabrication}\n"
-            f"Data de validade mais próxima: {near_fabrication}\n"
-            f"Empresa com mais produtos: {stock_company}"
+        oldest_date = ''
+        today_date = str(date.today())
+        list_expiration_date = []
+        list_company_name = []
+        list_manufacturing_date = []
+
+        for product in all_products:
+            # populo as listas primeiro
+            list_manufacturing_date.append(product['data_de_fabricacao'])
+
+            if product['data_de_validade'] > today_date:
+                list_expiration_date.append(product['data_de_validade'])
+
+            list_company_name.append(product['nome_da_empresa'])
+
+        # com elas cheias, guardo na variavel
+        oldest_date = min(list_manufacturing_date)
+
+        closest_expiration_date = min(list_expiration_date)
+
+        c = Counter(list_company_name)
+        most_repeated_name = c.most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_date}\n"
+            f"Data de validade mais próxima: {closest_expiration_date}\n"
+            f"Empresa com mais produtos: {most_repeated_name}"
         )
-        return generate_string
